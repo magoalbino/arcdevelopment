f84c4753306b17ff0bab1eb0856104490a07c7ee
Daniel <daniel.mileski@cidadania.gov.br>
2021-08-18 19:28:16 -0300

    Começando o curso

---
+++ src/components/ui/Header/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import {useScrollTrigger} from "@material-ui/core";
+
+interface Props {
+    children: React.ReactElement;
+}
+
+function ElevationScroll(props: Props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    })
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    })
+}
+
+export function Header() {
+    return (
+        <ElevationScroll>
+            <AppBar position="fixed">
+                <Toolbar>
+                    Arc Development
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
+    )
+}
