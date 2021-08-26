9994dd11472aa50ce6b397d64936c7f96e3d0dfe
Daniel <daniel.mileski@cidadania.gov.br>
2021-08-25 21:39:33 -0300

    Terminando Header

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import {useScrollTrigger} from "@material-ui/core";
+import React, { ChangeEvent, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {Button, Tab, Tabs, useScrollTrigger, Toolbar, AppBar} from "@material-ui/core";
+import {useStyles} from "./styles";
+
+import logo from '../../../assets/logo.svg';
 
 interface Props {
     children: React.ReactElement;
@@ -21,13 +23,62 @@ function ElevationScroll(props: Props) {
 }
 
 export function Header() {
+    const classes = useStyles();
+    const [TabValue, setTabValue] = useState(0);
+ 
+    const handleChangeTab = (e: ChangeEvent<{}>, value: number) => {
+        setTabValue(value);
+    }
+
+    useEffect( () => {
+        if (window.location.pathname === '/' && TabValue !== 0){
+            setTabValue(0)
+        } else if (window.location.pathname === '/services' && TabValue !== 1){ 
+            setTabValue(1)
+        } else if (window.location.pathname === '/revolution' && TabValue !== 1){ 
+            setTabValue(2)
+        } else if (window.location.pathname === '/about' && TabValue !== 1){ 
+            setTabValue(3)
+        } else if (window.location.pathname === '/contact' && TabValue !== 1){ 
+            setTabValue(4)
+        } else if (window.location.pathname === '/estimate' && TabValue !== 1){ 
+            setTabValue(5)
+        }
+    }, [])
+
     return (
-        <ElevationScroll>
-            <AppBar position="fixed">
-                <Toolbar>
-                    Arc Development
-                </Toolbar>
-            </AppBar>
-        </ElevationScroll>
+        <>
+            <ElevationScroll>
+                <AppBar position="fixed">
+                    <Toolbar disableGutters>
+                        <Button 
+                            component={Link} 
+                            to="/" 
+                            className={classes.logoContainer}
+                            onClick={ () => setTabValue(0)}
+                            disableRipple
+                        >
+                            <img src={logo} alt="company logo" className={classes.logo} />
+                        </Button>
+                        <Tabs 
+                            className={classes.tabContainer} 
+                            value={TabValue}
+                            onChange={handleChangeTab}
+                        >
+                            <Tab label="Home" component={Link} to="/" />
+                            <Tab label="Services" component={Link} to="/services" />
+                            <Tab label="The Revolution" component={Link} to="/revolution" />
+                            <Tab label="About Us" component={Link} to="/about" />
+                            <Tab label="Contact Us" component={Link} to="/contact" />
+                        </Tabs>
+                        <Button variant="contained" color="secondary"
+                        className={classes.button}>
+                            Free Estimate
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin}></div>
+        </>
     )
 }
---
+++ src/components/ui/Header/styles.ts
@@ -0,0 +1,49 @@
+import {makeStyles, createStyles} from "@material-ui/core/styles";
+
+// @ts-ignore
+export const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+    toolbarMargin: {
+        ...theme.mixins.toolbar,
+        marginBottom: "3rem"
+    },
+    logo: {
+        height: "7.2em"
+    },
+    tabContainer: {
+        marginLeft: 'auto',
+        '& .MuiButtonBase-root': { //Tab
+            ...theme.typography.tab,
+            minWidth: 10,
+            marginLeft: '25px',
+            lineHeight: '5.75rem',
+        }
+    },
+    button: { 
+        ...theme.typography.estimate,
+        borderRadius: '50px',
+        marginLeft: '50px',
+        marginRight: '25px',
+        height: '45px',
+    },
+    logoContainer: { 
+        padding: 0,
+        "&:hover": {
+            backgroundColor: 'transparent',
+        }
+    },
+}));
+
+
+//https://gist.github.com/Danilo-Araujo-Silva/2ce11fd0540dcc7eb3ad3e67fd75d02a
+
+/**
+ If you need more specificity it's best to simply chain more class names.
+
+ const Search = styled(TextInput)({
+  width: '100%',
+  '& input.MuiInputBase-input.MuiInput-input': {
+    background: '#ff00ff',
+  },
+})
+ */
